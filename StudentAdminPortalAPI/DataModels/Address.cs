1ef492e35586bc318d417ce96c9e6e0c7e625603
Chathura Munasinghe <mactmunasinghe@students.nsbm.ac.lk>
2023-04-10 16:29:24 +0530

    all the data models are created

---
+++ StudentAdminPortalAPI/DataModels/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentAdminPortalAPI.DataModels
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string PhysicalAddrss { get; set; }
+        public string PostalAddress { get; set; }
+
+        //navigation property
+
+        public Guid StudentId { get; set; }
+    }
+}
---
+++ StudentAdminPortalAPI/DataModels/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentAdminPortalAPI.DataModels
+{
+    public class Gender
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+    }
+}
